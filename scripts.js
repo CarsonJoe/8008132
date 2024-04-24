cda4f49d3f09fff4a810b3f0bd9bd3e379a30bd2
CarsonJoe <carsongragg@gmail.com>
2024-04-24 01:19:13 -0700

    Caluclator mostly finished except reset after equal, decimal and clear styling

---
+++ scripts.js
@@ -0,0 +1,101 @@
+
+
+let operation = []
+
+
+function registerClick(value){
+    console.log(value)
+
+    if (value === "clear") {
+        operation = []
+        output()
+        return
+    }
+
+    else if (operation.length === 0) {
+        if (isNum(value)) {
+            operation[0] = value
+        }
+    }
+
+    else if (operation.length === 1) {
+        if (isNum(value)) {
+            operation[0] += value
+        }
+        else if (value === "plusminus"){
+            operation[0] *= -1
+        }
+        else if ((value === ".") && (!(operation[0].includes('.')))){
+            operation[0] += value
+        }
+        else if (value === "percent"){
+            operation[0] *= .01
+        }
+        else {
+            operation[1] = value
+        }
+    }
+
+    else if (operation.length === 2) {
+        if (isNum(value)) {
+            operation[2] = value
+        }
+    }
+
+    else if (operation.length === 3) {
+        if (isNum(value)) {
+            operation[2] += value
+        } 
+        else if (value === "plusminus"){
+            operation[2] *= -1
+        }
+        else if ((value === ".") && (!(operation[2].includes('.')))){
+            operation[2] += value
+        }
+        else if (value === "percent"){
+            operation[2] *= .01
+        }
+        else if (value === "equals"){
+            console.log(operation[0])
+            let result = operate(parseInt(operation[0]), operation[1], parseInt(operation[2]))
+
+            operation = []
+            operation.push(result)
+            console.log(operation)
+        }
+    }
+
+    output()
+}
+
+function isNum(str){
+    return !isNaN(Number(str))
+}
+
+function output(){
+    document.getElementById("output").innerHTML = operation.join(' ')
+}
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        registerClick(button.id)
+    })
+})
+
+
+function operate(a, op, b) {
+    if(op==="+") {
+        return(a+b)
+    }
+    if(op==="-") {
+        return(a-b)
+    }
+    if(op==="/") {
+        return(a/b)
+    }
+    if(op==="*") {
+        return(a*b)
+    }
+}
